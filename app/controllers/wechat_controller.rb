979402e469b49968a26a394017e02bc8fe0e3d52
samqiu <kinsamqiu@gmail.com>
2015-01-26 14:06:20 +0800

    wip

--- app/controllers/wechat_controller.rb
+++ app/controllers/wechat_controller.rb
@@ -1,45 +1,62 @@
 class WechatController < ApplicationController
+  before_action :set_keys
+
+  def auth_url
+    "https://open.weixin.qq.com/connect/oauth2/authorize?appid=#{@app_id}&redirect_uri=#{wechat_auth_return_url}&response_type=code&scope=snsapi_userinfo&state=123"
+  end
 
   def auth_return
     code = params[:code]
+
+    url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=#{@app_id}&secret=#{@app_secret}&code=#{code}&grant_type=authorization_code"
+    response = RestClient.get(url)
+
+    Rails.logger.info(response)
+
+    open_id = response['openid']
+    cookies[:open_id] = {value: open_id, httponly: true}
+
+    redirect_to wechat_url
   end
 
   def index
     @search = BaseDrugHosp.new
-    open_id = 'ob56ojj3_OsMkNue9870yWX_xBfU'
-    appid = 'wxcbb786241f2ef879'
-    mch_id = '1228019202'
-    options = {
-        appid: appid,
-        mch_id: mch_id,
-        nonce_str: SecureRandom.uuid.gsub('-',''),
-        body: 'test',
-        out_trade_no: '10001',
-        total_fee: 1,
-        spbill_create_ip: request.remote_ip,
-        notify_url: wechat_notify_url,
-        trade_type: 'JSAPI',
-        openid: open_id
-    }
-    @payload = make_payload(options)
-    response = RestClient::Request.execute(
-        {
-            method: :post,
-            url: 'https://api.mch.weixin.qq.com/pay/unifiedorder',
-            payload: @payload,
-            headers: {content_type: 'application/xml'}
-        }
-    )
-    @response = Hash.from_xml(response)
 
-    @options = {
-      appId: appid,
-      timeStamp: Time.now.to_i.to_s,
-      nonceStr: @response['xml']['nonce_str'],
-      package: "prepay_id=#{@response['xml']['prepay_id']}",
-      signType: 'MD5'
-    }
-    @options[:paySign] = generate_sign(@options)
+    if cookies[:open_id]
+      options = {
+          appid: @app_id,
+          mch_id: @mch_id,
+          nonce_str: SecureRandom.uuid.gsub('-', ''),
+          body: 'test',
+          out_trade_no: '10001',
+          total_fee: 1,
+          spbill_create_ip: request.remote_ip,
+          notify_url: wechat_notify_url,
+          trade_type: 'JSAPI',
+          openid: cookies[:open_id]
+      }
+      @payload = make_payload(options)
+      response = RestClient::Request.execute(
+          {
+              method: :post,
+              url: 'https://api.mch.weixin.qq.com/pay/unifiedorder',
+              payload: @payload,
+              headers: {content_type: 'application/xml'}
+          }
+      )
+      @response = Hash.from_xml(response)
+
+      @options = {
+          appId: @app_id,
+          timeStamp: Time.now.to_i.to_s,
+          nonceStr: @response['xml']['nonce_str'],
+          package: "prepay_id=#{@response['xml']['prepay_id']}",
+          signType: 'MD5'
+      }
+      @options[:paySign] = generate_sign(@options)
+    else
+      redirect_to auth_url
+    end
 
   end
 
@@ -52,9 +69,15 @@ class WechatController < ApplicationController
     query = options.sort.map do |key, value|
       "#{key}=#{value}"
     end.join('&')
-    
+
     query += '&key=YfP5yCNVzsYffsfy64hmKGuqcl3xop3A'
     Digest::MD5.hexdigest(query).upcase
+  end
+
+  def set_keys
+    @app_id = 'wxcbb786241f2ef879'
+    @mch_id = '1228019202'
+    @app_secret = '0312da9137d5e5bdc71a14eed8816ffc'
   end
 
 end
