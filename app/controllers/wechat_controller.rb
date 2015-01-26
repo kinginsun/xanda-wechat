588972f5828f3c5b474d38c7443d0d1339e0307c
samqiu <kinsamqiu@gmail.com>
2015-01-26 13:34:37 +0800

    Debug wechat payment

--- app/controllers/wechat_controller.rb
+++ app/controllers/wechat_controller.rb
@@ -6,20 +6,20 @@ class WechatController < ApplicationController
 
   def index
     @search = BaseDrugHosp.new
-
+    open_id = 'ob56ojj3_OsMkNue9870yWX_xBfU'
+    appid = 'wxcbb786241f2ef879'
+    mch_id = '1228019202'
     options = {
-        appid: 'wxcbb786241f2ef879',
-        mch_id: '1228019202',
-        # key: '0312da9137d5e5bdc71a14eed8816ffc',
-
-        nonce_str: '6da569638aec4855b3d3b44ec1103e2c',
+        appid: appid,
+        mch_id: mch_id,
+        nonce_str: SecureRandom.uuid.gsub('-',''),
         body: 'test',
         out_trade_no: '10001',
         total_fee: 1,
         spbill_create_ip: request.remote_ip,
         notify_url: wechat_notify_url,
         trade_type: 'JSAPI',
-        openid: '99999'
+        openid: open_id
     }
     @payload = make_payload(options)
     response = RestClient::Request.execute(
@@ -31,10 +31,19 @@ class WechatController < ApplicationController
         }
     )
     @response = Hash.from_xml(response)
+
+    @options = {
+      appId: appid,
+      timeStamp: Time.now.to_i.to_s,
+      nonceStr: @response['xml']['nonce_str'],
+      package: "prepay_id=#{@response['xml']['prepay_id']}",
+      signType: 'MD5'
+    }
+    @options[:paySign] = generate_sign(@options)
+
   end
 
   private
-
   def make_payload(options)
     "<xml>#{options.sort.map { |k, v| "<#{k}>#{v}</#{k}>" }.join}<sign>#{generate_sign(options)}</sign></xml>"
   end
@@ -43,7 +52,8 @@ class WechatController < ApplicationController
     query = options.sort.map do |key, value|
       "#{key}=#{value}"
     end.join('&')
-    query += '&key=0312da9137d5e5bdc71a14eed8816ffc'
+    
+    query += '&key=YfP5yCNVzsYffsfy64hmKGuqcl3xop3A'
     Digest::MD5.hexdigest(query).upcase
   end
 
